a529edea3578b41cee093ab304570c6204a8f6c5
Shawn <121857051@qq.com>
2019-01-15 11:25:36 +0800

    update: ControlFlow

--- src/main/julia/basecases/ControlFlow.jl
+++ src/main/julia/basecases/ControlFlow.jl
@@ -6,6 +6,7 @@ ControlFlow:
 =#
 
 # Compound
+println("Compound:")
 z = begin
     x=1
     y=2
@@ -17,6 +18,7 @@ c = (a=2;b=19;a*b)
 println(c)
 
 # Conditional
+println("Conditional: ")
 function f(x,y)
     if x<y
     println("$x is smaller than $y")
@@ -32,6 +34,7 @@ f(2,1)
 f(0,0)
 
 # Loops
+println("Loops:")
 j=1
 while j <= 5
     println(j)
@@ -44,4 +47,40 @@ end
 
 for j in ["banana","Banana"]
     println(j)
+end
+
+# Control
+println("Control:")
+for i in [1,2,3,4,5]
+    if i == 2
+    continue
+    end
+    if i == 4
+    break
+    end
+    println(i)
+end
+
+# Exception
+println("Exception: ")
+
+try
+    a=sqrt(-2)
+    println("try")
+catch
+    println("catch")
+finally
+    println("finally")
+end
+
+try
+    throw(AssertionError("banana"))
+catch x
+    println(x)
+end
+
+try
+    error("this is an error")
+catch e
+    println(e)
 end
